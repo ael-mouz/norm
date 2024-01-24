48d82d3d4ee139a4a7f11b5398d6a58aed7769b6
ael-mouz <ael-mouz@student.1337.ma>
2024-01-24 22:41:14 +0100

    add command norm and fix_norm

--- install.sh
+++ install.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-# Color codes
 GREEN='\033[0;32m'
 RED='\033[0;31m'
-NC='\033[0m' # No Color
+NC='\033[0m'
 
-# Save the norm script
-cat > norm.sh << 'EOF'
+cat > ~/.norm/norm.sh << 'EOF'
 #!/bin/bash
 
-# Color codes
 GREEN='\033[0;32m'
 RED='\033[0;31m'
-NC='\033[0m' # No Color
+NC='\033[0m'
 
 parent_dir="."  # Change this to the desired parent directory
 
@@ -20,27 +17,58 @@ norminette_files() {
     find "$parent_dir" -type f \( -name "*.c" -o -name "*.h" \) | while read -r file; do
         output=$(norminette "$file" 2>&1)
         if [ $? -eq 0 ]; then
-            printf "${GREEN}File: %s${NC}" "$output"
+            printf "${GREEN}File: %s ✔️${NC}\n" "$file"
         else
-            printf "${RED}File: %s${NC}" "$output"
+            printf "${RED}File: %s${NC}\n" "$file"
         fi
-        echo
     done
 }
 
 norminette_files
 EOF
 
-# Move the norm script to a directory in PATH
+cat > ~/.norm/norm_.sh << 'EOF'
+#!/bin/bash
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+python3 -m venv venv
+source venv/bin/activate
+python3 -m pip install --upgrade pip
+pip3 install c-formatter-42
+COMMAND="c_formatter_42"
+
+norm() {
+    find . -type f \( -name "*.c" -o -name "*.h" \) | while read -r FILE; do
+        TMP_FILE="${FILE}.tmp"
+        $COMMAND < "$FILE" > "$TMP_FILE" 2>&1
+        if [ $? -eq 0 ]; then
+            mv "$TMP_FILE" "$FILE"
+            printf "${GREEN}Success${NC} - ${FILE} ✔️\n"
+        else
+            printf "${RED}Error${NC} - ${FILE}\n"
+            rm "$TMP_FILE"
+        fi
+    done
+    echo "Processing complete."
+}
+
+deactivate
+rm -rf venv
+EOF
+
 mkdir -p ~/.norm
-mv norm.sh ~/.norm
-
-# Make the script executable
 chmod +x ~/.norm/norm.sh
+chmod +x ~/.norm/norm_.sh
 
-# Add the alias to .bashrc (Bash) or .zshrc (Zsh)
 echo "alias norm=\"~/.norm/norm.sh\"" >> ~/.bashrc
 echo "alias norm=\"~/.norm/norm.sh\"" >> ~/.zshrc
+echo "alias fix_norm=\"~/.norm/norm_.sh\"" >> ~/.bashrc
+echo "alias fix_norm=\"~/.norm/norm_.sh\"" >> ~/.zshrc
 
-echo -e "${GREEN}The 'norm' tool has been installed successfully.${NC}"
-echo -e "${GREEN}You can now use 'norm' command to run the norminette on files.${NC}"
+echo -e "${GREEN}The 'norm' tool has been installed successfully. ✔️${NC}"
+echo -e "${GREEN}The 'fix_norm' tool has been installed successfully. ✔️${NC}"
+echo -e "You can now use 'norm' command to run the norminette on files."
+echo -e "You can now use 'fix_norm' command to fix the norminette on files."
